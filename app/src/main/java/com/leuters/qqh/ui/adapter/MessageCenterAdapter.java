7a0cf143ed036a4acf886d2c32fe508162ec60c5
yuqingKitty <582650269@qq.com>
2019-08-08 00:21:27 +0800

    v1.0创建签名。

--- app/src/main/java/com/leuters/qqh/ui/adapter/MessageCenterAdapter.java
+++ app/src/main/java/com/leuters/qqh/ui/adapter/MessageCenterAdapter.java
@@ -38,12 +38,12 @@ public class MessageCenterAdapter extends BaseAdapter<MessageCenterBean.MessageB
             @Override
             public void onClick(View v) {
                 if (helper.getView(R.id.tv_message_desc).getVisibility() == View.VISIBLE) {
-                    // 显示到不显示
-                    helper.setImageResource(R.id.iv_message_arrow, R.mipmap.icon_arrow_down);
+                    // 显示到不显示 down
+                    helper.setImageResource(R.id.iv_message_arrow, R.mipmap.icon_arrow_right);
                     helper.setGone(R.id.tv_message_desc, false);
                 } else {
-                    // 不显示到显示
-                    helper.setImageResource(R.id.iv_message_arrow, R.mipmap.icon_arrow_up);
+                    // 不显示到显示 up
+                    helper.setImageResource(R.id.iv_message_arrow, R.mipmap.icon_arrow_right);
                     helper.setGone(R.id.tv_message_desc, true);
                 }
             }
